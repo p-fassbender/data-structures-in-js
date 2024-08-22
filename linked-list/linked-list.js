c0883c5f291435f891bf154fc6735a688c393731
Preston Fassbender <fassbenderp0551@gmail.com>
2024-08-22 12:18:10 -0500

    linked list reverse

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -124,6 +124,18 @@ class LinkedList {
         return -1
     }
 
+    reverse() {
+        let prev = null
+        let curr = this.head
+        while (curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        this.head = prev
+    }
+
     print() {
         if (this.isEmpty()) {
             console.log("the list is empty")
@@ -190,3 +202,8 @@ console.log("list size: ", list.getSize())
 console.log("- SEARCH VALUE -")
 console.log(list.search(2))
 console.log(list.search("b"))
+
+console.log("- REVERSE -")
+list.print()
+list.reverse()
+list.print()
